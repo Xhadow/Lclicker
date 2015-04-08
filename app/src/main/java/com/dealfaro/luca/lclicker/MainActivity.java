ee776438f7d85f50a0e30cdf6350fad2ebc74499
Xhadow <grcabrer@ucsc.edu>
2015-04-08 16:46:17 -0700

    Finished save, APK gen

--- app/src/main/java/com/dealfaro/luca/lclicker/MainActivity.java
+++ app/src/main/java/com/dealfaro/luca/lclicker/MainActivity.java
@@ -21,9 +21,27 @@ public class MainActivity extends ActionBarActivity {
         Button b = (Button) v;
         String t = b.getText().toString();
         TextView tv = (TextView) findViewById(R.id.textView);
-        tv.setText(t);
+        //Check to make sure there aren't 12 numbers already
+        if((tv.getText().toString().length()) < 12) {
+            //Add number to string
+            tv.setText(tv.getText().toString() + t);
+        }
     }
 
+    public void clickDELButton(View v) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        //Check to see that numbers are not at 0
+        if((tv.getText().toString().length()) > 0) {
+            //Subtract a number
+            tv.setText(tv.getText().toString().substring(0, (tv.getText().toString().length()) - 1));
+        }
+    }
+
+    public void clickCALLButton(View v) {
+        String t = "";
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(t);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
